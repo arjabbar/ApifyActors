3f693676010e31894a549ddb76b9b5d94954c20b
AJ <ajabbar@platesmart.com>
2023-01-02 19:12:26 -0600

    Got the thing working.

--- hair-product-scraper/src/main.ts
+++ hair-product-scraper/src/main.ts
@@ -5,24 +5,36 @@
  */
 
 // For more information, see https://sdk.apify.com
-import { Actor } from 'apify';
+import { Actor, log } from 'apify';
 // For more information, see https://crawlee.dev
-import { PlaywrightCrawler } from 'crawlee';
+import { PlaywrightCrawler, Dataset } from 'crawlee';
 import { router } from './routes.js';
 
 // Initialize the Apify SDK
 await Actor.init();
 
-const startUrls = ['https://apify.com'];
+const startUrls = ['https://www.amazon.com/Hair-Care-Products-Beauty/b/?ie=UTF8&node=11057241'];
 
-const proxyConfiguration = await Actor.createProxyConfiguration();
+const proxyConfiguration = await Actor.createProxyConfiguration({
+    useApifyProxy: !!process.env.USE_PROXY,
+});
 
 const crawler = new PlaywrightCrawler({
     proxyConfiguration,
     requestHandler: router,
+    headless: false,
+    postNavigationHooks: [
+        async ({ page }) => {
+            // debugger;
+        },
+    ],
 });
 
 await crawler.run(startUrls);
+
+await Dataset.exportToJSON('data', {
+    toKVS: 'data',
+});
 
 // Exit successfully
 await Actor.exit();
--- hair-product-scraper/src/routes.ts
+++ hair-product-scraper/src/routes.ts
@@ -1,21 +1,57 @@
 import { Dataset, createPlaywrightRouter } from 'crawlee';
 
+const MAX_PAGES = Number.parseInt(process.env.MAX_PAGES!, 10) ?? 5;
+const MAX_ITEMS_PER_PAGE = Number.parseInt(process.env.MAX_ITEMS_PER_PAGE!, 10) ?? 5;
+
 export const router = createPlaywrightRouter();
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
-    await enqueueLinks({
-        globs: ['https://apify.com/*'],
-        label: 'detail',
+router.addDefaultHandler(async ({ log, page, enqueueLinksByClickingElements }) => {
+    await enqueueLinksByClickingElements({
+        selector: '[title=Shampoos]',
+        label: 'subcategory',
     });
 });
 
-router.addHandler('detail', async ({ request, page, log }) => {
+router.addHandler('subcategory', async ({ request, page, log, enqueueLinks }) => {
     const title = await page.title();
-    log.info(`${title}`, { url: request.loadedUrl });
+    const pageNumber = request.userData.pageNumber ?? 1;
 
-    await Dataset.pushData({
-        url: request.loadedUrl,
-        title,
+    log.info(`${title}`, { url: request.loadedUrl });
+    log.info(`On page ${pageNumber} of ${MAX_PAGES}. Capturing ${MAX_ITEMS_PER_PAGE} items on this page.`);
+
+    await enqueueLinks({
+        selector: '[data-component-type=s-product-image] > a',
+        label: 'product',
+        limit: MAX_ITEMS_PER_PAGE,
     });
+
+    if (pageNumber >= MAX_PAGES) {
+        return;
+    }
+
+    await enqueueLinks({
+        selector: '.s-pagination-next',
+        label: 'subcategory',
+        userData: {
+            pageNumber: pageNumber + 1,
+        },
+    });
+});
+
+router.addHandler('product', async ({ request, page, log }) => {
+    const title = (await page.innerText('#productTitle')).trim();
+    const productDetailsString = await page.innerText('[data-feature-name=productOverview]');
+    const imageSrc = await page.getAttribute('[data-a-image-name=landingImage]', 'src');
+    const details: any = {};
+
+    productDetailsString?.split('\n').forEach((line) => {
+        const splitLine = line.split('\t');
+        details[splitLine[0]] = splitLine[1];
+    });
+
+    const product = { title, imageSrc, details };
+
+    log.info('Storing product', product);
+
+    await Dataset.pushData(product);
 });
