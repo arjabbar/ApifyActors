e50c405e1cfa239e44946fdbeaebe3508b3fd303
AJ <ajabbar@platesmart.com>
2023-01-05 20:37:12 -0600

    Fixed the followup actor call

--- hair-product-scraper/src/main.ts
+++ hair-product-scraper/src/main.ts
@@ -5,7 +5,7 @@
  */
 
 // For more information, see https://sdk.apify.com
-import { Actor } from 'apify';
+import { Actor, Dataset } from 'apify';
 // For more information, see https://crawlee.dev
 import { PlaywrightCrawler, Dictionary } from 'crawlee';
 import { router } from './routes.js';
@@ -38,10 +38,13 @@ const crawler = new PlaywrightCrawler({
 
 await crawler.run(startUrls);
 
+const dataset = await Dataset.open(input!.datasetName);
+const datasetInfo = await dataset.getInfo();
+
 if (input?.callImageDownloader) {
     const imageDownloaderInput = {
         convertWebpToPng: false,
-        datasetId: input?.datasetName ?? undefined,
+        datasetId: datasetInfo?.id,
         fileNameFunction: '({url, md5}) => md5(url)',
         imageCheckType: 'content-type',
         noDownloadRun: false,
