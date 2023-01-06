80c8021858eb4848af2453b211e95927104099ef
AJ <ajabbar@platesmart.com>
2023-01-05 19:55:49 -0600

    Added conditional image downlaoder hook.

--- hair-product-scraper/src/main.ts
+++ hair-product-scraper/src/main.ts
@@ -7,16 +7,18 @@
 // For more information, see https://sdk.apify.com
 import { Actor } from 'apify';
 // For more information, see https://crawlee.dev
-import { PlaywrightCrawler, Dataset } from 'crawlee';
+import { PlaywrightCrawler, Dictionary } from 'crawlee';
 import { router } from './routes.js';
 
 // Initialize the Apify SDK
 await Actor.init();
 
+const input = await Actor.getInput<Dictionary>();
+
 const startUrls = ['https://www.amazon.com/Hair-Care-Products-Beauty/b/?ie=UTF8&node=11057241'];
 
 const proxyConfiguration = await Actor.createProxyConfiguration({
-    useApifyProxy: !!process.env.USE_PROXY,
+    useApifyProxy: !!input?.useProxy,
 });
 
 const crawler = new PlaywrightCrawler({
@@ -27,14 +29,34 @@ const crawler = new PlaywrightCrawler({
             // debugger;
         },
     ],
+    failedRequestHandler: (ctx, err) => {
+        ctx.log.error(err.message, { url: ctx.page.url(), error: err });
+        ctx.saveSnapshot({ saveHtml: true, saveScreenshot: true, key: ctx.page.url().replace(/[^a-z0-9]/gi, '_').toLowerCase() });
+    },
     maxConcurrency: 2,
 });
 
 await crawler.run(startUrls);
 
-await Dataset.exportToJSON('data', {
-    toKVS: 'data',
-});
+if (input?.callImageDownloader) {
+    const imageDownloaderInput = {
+        convertWebpToPng: false,
+        datasetId: input?.datasetName ?? undefined,
+        fileNameFunction: '({url, md5}) => md5(url)',
+        imageCheckType: 'content-type',
+        noDownloadRun: false,
+        outputTo: 'no-output',
+        pathToImageUrls: 'images',
+        proxyConfiguration: {
+            useApifyProxy: true,
+        },
+        s3CheckIfAlreadyThere: false,
+        uploadStoreName: 'productImages',
+        uploadTo: 'key-value-store',
+    };
+
+    await Actor.call('lukaskrivka/images-download-upload', imageDownloaderInput);
+}
 
 // Exit successfully
 await Actor.exit();
--- hair-product-scraper/src/routes.ts
+++ hair-product-scraper/src/routes.ts
@@ -55,32 +55,37 @@ router.addHandler('product', async ({ request, page, log }) => {
     log.info(`Details Bullet`, { detailBullets });
 
     const details: any = {
-        ...getDetailsFromMultilineString(detailBullets),
-        ...getDetailsFromMultilineString(productOverview),
+        ...(await getDetailsFromMultilineString(detailBullets)),
+        ...(await getDetailsFromMultilineString(productOverview)),
     };
 
     const product = { title, images, details, url: request.loadedUrl };
 
     log.info('Storing product', product);
 
-    await Dataset.pushData(product);
+    const input = await Actor.getInput<Dictionary>();
+    const dataset = await Dataset.open(input!.datasetName);
+    await dataset.pushData(product);
 });
 
-function getDetailsFromMultilineString(detailsStr: string) {
+async function getDetailsFromMultilineString(detailsStr: string) {
     const parsedDetails = {} as any;
     if (!detailsStr) {
         return;
     }
+
+    const input = await Actor.getInput<Dictionary>();
+
     detailsStr.split('\n').forEach((line) => {
-        const splitLine = line.split(/\t|:/gi);
+        const splitLine = line.split(new RegExp(input?.detailsSplitRegex ?? /\t|(\W*:\W*)/gi, 'gi'));
         // If we parsed air, do nothing
-        if (!splitLine[0] || !splitLine[1]) {
+        if (!splitLine[0] || !splitLine[splitLine.length - 1]) {
             return;
         }
 
         // Remove non-word characters from keys and values. ex. "Is Discontinued " => "IsDiscontinued"
-        const detailName = splitLine[0].trim().replace(/\W/g, '');
-        const detailValue = splitLine[1].trim();
+        const detailName = splitLine[0].trim();
+        const detailValue = splitLine[splitLine.length - 1].trim();
 
         if (!detailName || !detailValue) {
             return;
